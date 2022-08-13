589e7563ab2f55a644e76876157716eaf97b64e8
Rajasekar <Rajasekar.Subramanian@in.bosch.com>
2022-08-13 11:53:20 +0530

    date picker integration

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,51 @@
+import { NgxMatDateAdapter, NgxMatDateFormats, NgxMatDatetimePickerModule, NGX_MAT_DATE_FORMATS } from '@angular-material-components/datetime-picker';
 import { NgModule } from '@angular/core';
+import { MAT_MOMENT_DATE_ADAPTER_OPTIONS } from '@angular/material-moment-adapter';
+import { MAT_DATE_LOCALE } from '@angular/material/core';
+import { MatDatepickerModule } from '@angular/material/datepicker';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DemoComponent } from './demo/demo.component';
+import { NgxMatMomentAdapter } from '@angular-material-components/moment-adapter';
+import { MatInputModule } from '@angular/material/input';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
 
+const MY_NGX_DATE_FORMATS: NgxMatDateFormats = {
+  parse: {
+    dateInput: "l, LTS"
+  },
+  display: {
+    dateInput: "DD.MM.yyyy HH:mm",
+    monthYearLabel: "MMM YYYY",
+    dateA11yLabel: "LL",
+    monthYearA11yLabel: "MMMM YYYY"
+  }
+};
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DemoComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    MatDatepickerModule,
+    NgxMatDatetimePickerModule,
+    MatInputModule,
+    BrowserAnimationsModule
   ],
-  providers: [],
+  providers: [{
+    provide: NgxMatDateAdapter,
+    useClass: NgxMatMomentAdapter,
+    deps: [MAT_DATE_LOCALE, MAT_MOMENT_DATE_ADAPTER_OPTIONS]
+  },
+  { provide: NGX_MAT_DATE_FORMATS, useValue: MY_NGX_DATE_FORMATS },
+],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
---
+++ src/readme.txt
@@ -0,0 +1,7 @@
+npm install --save  @angular-material-components/datetime-picker
+npm install --save @angular/material
+npm install --save @angular/material-moment-adapter
+npm install @angular-material-components/moment-adapter
+npm install moment
+--npm install @angular/animations
+npm install @angular/platform-browser
